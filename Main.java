e9181f2320a229430749f55a803f9786e75e6a7f
Myudro0 <147828569+Myudro0@users.noreply.github.com>
2023-10-13 17:51:44 +0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,39 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        /*int[] test = new int[20];
+        for (int i = 0; i < test.length; i++){
+
+            test[i] = i * 3;
+        }
+
+        for (int j : test) {
+            if (j % 2 == 0) {
+                System.out.println(j);
+            }
+        }
+
+        int counter = 0;
+        for (int num : test) {
+            if (counter % 2 == 1) System.out.println(counter);
+            counter++;
+        }*/
+
+        /*3 assignment*/
+        System.out.println("                                 ");
+        System.out.println("     J     A     V     V    A    ");
+        System.out.println("     J    A A     V   V    A A   ");
+        System.out.println(" J   J   AAAAA     V V    AAAAA  ");
+        System.out.println("  J J   A     A     V    A     A ");
+        System.out.println("                                 ");
+
+        /*4 assignment*/
+        System.out.println("a     a^2     a^3");
+        System.out.println("1     1       1");
+        System.out.println("2     4       8");
+        System.out.println("3     9       27");
+        System.out.println("4     16      64");
+    }
+}
